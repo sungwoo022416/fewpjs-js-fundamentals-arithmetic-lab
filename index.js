a8d623b192a84f493f2464efd2f663329b3b1ef1
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-12 06:26:18 -0700

    commit tree c2b34917f9d043a99606228634732b192dec0266

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1* num2;
+console.log(multiply)
+
+let random = Math.floor(Math.random()*10) + 1;
+console.log(random)
+
+let num3 = 5
+let num4 = 9
+let mod = num4 % num3;
+console.log(mod)
+
+let max = Math.max(20, 1, 2, 3, 4, 5);
+console.log(max)
